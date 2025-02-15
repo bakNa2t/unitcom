b4144f09fdbcca5f124648798840332866e82163
bakNa2t <ok2.lock@gmail.com>
2025-02-15 13:43:17 +0300

    Add ChatsBlock

--- app/chats/layout.tsx
+++ app/chats/layout.tsx
@@ -1,14 +1,16 @@
 import { ReactNode } from "react";
 
+import { ChatsBlock } from "../_components/ChatsBlock";
+
 interface ChatLayoutProps {
   children: ReactNode;
 }
 
-const ChatLayout = ({ children }: ChatLayoutProps) => {
+const ChatLayout = ({ children }: { children: ChatLayoutProps }) => {
   return (
     <>
       <div className="hidden md:flex md:ml-24 px-2 md:px-0 h-dvh">
-        {children}
+        <ChatsBlock>{children}</ChatsBlock>
       </div>
 
       <div className="md:hidden my-20 md:px-2 bg-slate-200">{children}</div>

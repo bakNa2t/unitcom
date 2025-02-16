6d84373b8590507b9880d79e37a845226b8d6d14
bakNa2t <ok2.lock@gmail.com>
2025-02-16 18:23:22 +0300

    Add ResizablePanelGroup

--- app/_components/SharedBlock.tsx
+++ app/_components/SharedBlock.tsx
@@ -3,13 +3,23 @@
 import { FC, ReactNode, useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+
 type SharedBlockProps = {
   children: ReactNode;
   SidebarComponent: FC<any>;
   SidebarProps?: any;
 };
 
-export const SharedBlock: FC<SharedBlockProps> = ({ children }) => {
+export const SharedBlock: FC<SharedBlockProps> = ({
+  children,
+  SidebarComponent,
+  SidebarProps,
+}) => {
   const pathName = usePathname();
   const [isRendered, setIsRendered] = useState(false);
 
@@ -21,6 +31,17 @@ export const SharedBlock: FC<SharedBlockProps> = ({ children }) => {
 
   return (
     <>
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel defaultSize={30} maxSize={40}>
+          <SidebarComponent {...SidebarProps} />
+        </ResizablePanel>
+
+        <ResizableHandle
+          withHandle={pathName === "/chats"}
+          className="border-r border-r-gray-400 dark:border-r-gray-800"
+        />
+      </ResizablePanelGroup>
+
       <div className="md:hidden">{children}</div>
     </>
   );

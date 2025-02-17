1f4917ceaeae5005526613867ed9320906a86afb
bakNa2t <ok2.lock@gmail.com>
2025-02-17 12:03:57 +0300

    Move components

--- app/chats/_components/ChatsBlock.tsx
+++ app/chats/_components/ChatsBlock.tsx
@@ -2,11 +2,16 @@
 
 import { FC, ReactNode } from "react";
 import { SharedBlock } from "./SharedBlock";
+import ChatSidebar from "./ChatSidebar";
 
 type ChatsBlockProps = {
   children: ReactNode;
 };
 
 export const ChatsBlock: FC<ChatsBlockProps> = ({ children }) => {
-  return <SharedBlock SidebarComponent={() => <></>}>{children}</SharedBlock>;
+  return (
+    <SharedBlock SidebarComponent={() => <ChatSidebar />}>
+      {children}
+    </SharedBlock>
+  );
 };

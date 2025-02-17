3f1a072d3064aff32c8141745c1d9b187eac9564
bakNa2t <ok2.lock@gmail.com>
2025-02-17 16:04:18 +0300

    Add Dialog to navigation bar

--- app/chats/_components/NavigationBar.tsx
+++ app/chats/_components/NavigationBar.tsx
@@ -17,6 +17,7 @@ import {
 } from "@/components/ui/tooltip";
 
 import { cn, isRouteActivePath } from "@/lib/utils";
+import { Dialog } from "@/components/ui/dialog";
 
 type NavigationBarProps = {
   trigger: ReactNode;
@@ -27,47 +28,65 @@ export const NavigationBar: FC<NavigationBarProps> = () => {
 
   const menuItems = useMemo(
     () => [
-      { icon: MessageCircle, label: "Chat", path: "/cahts" },
+      { icon: MessageCircle, label: "Chat", path: "/chats" },
       { icon: Phone, label: "Call", path: "/calls" },
     ],
     []
   );
 
   return (
-    <div className="flex md:flex-col items-center justify-between bottom-0 md:top-0 left-0 md:w-24 w-full md:h-screen h-20 bg-white dark:bg-slate-950 border-r md:border-r-gray-400 md:dark:border-r-gray-800 py-5 fixed z-10 ">
-      <div className="md:pt-10">
-        <NavigationMenu orientation="vertical">
-          <NavigationMenuList className="flex md:flex-col items-center justify-between !w-svw md:!w-fit px-5 md:px-0 md:space-y-4">
-            {menuItems.map(({ icon: Icon, label, path }) => {
-              const isActive = isRouteActivePath(pathname, path);
+    <>
+      <div className="flex md:flex-col items-center justify-between bottom-0 md:top-0 left-0 md:w-24 w-full md:h-screen h-20 bg-white dark:bg-slate-950 border-r md:border-r-gray-400 md:dark:border-r-gray-800 py-5 fixed z-10 ">
+        <div className="md:pt-10">
+          <NavigationMenu orientation="vertical">
+            <NavigationMenuList className="flex md:flex-col items-center justify-between !w-svw md:!w-fit px-5 md:px-0 md:space-y-4">
+              {menuItems.map(({ icon: Icon, label, path }) => {
+                const isActive = isRouteActivePath(pathname, path);
 
-              return (
-                <Tooltip key={path}>
-                  <TooltipTrigger>
-                    <Link href={path}>
-                      <NavigationMenuItem
-                        className={cn("px-5 py-2 cursor-pointer rounded-xl", {
-                          "dark:bg-gray-700 bg-gray-300": isActive,
-                          "hover:dark:bg-gray-600 hover:bg-gray-200": !isActive,
-                        })}
-                      >
-                        <Icon size={18} fill={isActive ? "#131313" : "none"} />
-                      </NavigationMenuItem>
-                    </Link>
-                  </TooltipTrigger>
+                return (
+                  <Tooltip key={path}>
+                    <TooltipTrigger>
+                      <Link href={path}>
+                        <NavigationMenuItem
+                          className={cn("px-5 py-2 cursor-pointer rounded-xl", {
+                            "dark:bg-gray-700 bg-gray-300": isActive,
+                            "hover:dark:bg-gray-600 hover:bg-gray-200":
+                              !isActive,
+                          })}
+                        >
+                          <Icon
+                            size={18}
+                            fill={isActive ? "#131313" : "none"}
+                          />
+                        </NavigationMenuItem>
+                      </Link>
+                    </TooltipTrigger>
 
-                  <TooltipContent
-                    side="right"
-                    className="bg-gray-50 dark:bg-gray-700 rounded-xl"
-                  >
-                    <p>{label}</p>
-                  </TooltipContent>
-                </Tooltip>
-              );
-            })}
+                    <TooltipContent
+                      side="right"
+                      className="bg-gray-500 dark:bg-gray-700 rounded-xl"
+                    >
+                      <p>{label}</p>
+                    </TooltipContent>
+                  </Tooltip>
+                );
+              })}
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+      </div>
+
+      <div className="md:hidden fixed flex justify-between w-svw top-0 left-0 h-20 z-10 px-10 bg-white dark:bg-slate-950">
+        <NavigationMenu orientation="horizontal">
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <Dialog>
+                <NavigationMenu orientation="vertical"></NavigationMenu>
+              </Dialog>
+            </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
       </div>
-    </div>
+    </>
   );
 };

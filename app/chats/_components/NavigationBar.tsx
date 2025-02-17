5bd3a6836a64dbcad0b323f833be03f69b8f452f
bakNa2t <ok2.lock@gmail.com>
2025-02-17 14:48:41 +0300

    Add NavigationMenu and Tooltip to navigation

--- app/chats/_components/NavigationBar.tsx
+++ app/chats/_components/NavigationBar.tsx
@@ -3,13 +3,27 @@
 import { FC, ReactNode, useMemo } from "react";
 import { usePathname } from "next/navigation";
 import { MessageCircle, Phone } from "lucide-react";
+import Link from "next/link";
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import { cn, isRouteActivePath } from "@/lib/utils";
 
 type NavigationBarProps = {
   trigger: ReactNode;
 };
 
 export const NavigationBar: FC<NavigationBarProps> = () => {
-  const pathName = usePathname();
+  const pathname = usePathname();
 
   const menuItems = useMemo(
     () => [
@@ -21,7 +35,39 @@ export const NavigationBar: FC<NavigationBarProps> = () => {
 
   return (
     <div className="flex md:flex-col items-center justify-between bottom-0 md:top-0 left-0 md:w-24 w-full md:h-screen h-20 bg-white dark:bg-slate-950 border-r md:border-r-gray-400 md:dark:border-r-gray-800 py-5 fixed z-10 ">
-      <div className="md:pt-10"></div>
+      <div className="md:pt-10">
+        <NavigationMenu orientation="vertical">
+          <NavigationMenuList className="flex md:flex-col items-center justify-between !w-svw md:!w-fit px-5 md:px-0 md:space-y-4">
+            {menuItems.map(({ icon: Icon, label, path }) => {
+              const isActive = isRouteActivePath(pathname, path);
+
+              return (
+                <Tooltip key={path}>
+                  <TooltipTrigger>
+                    <Link href={path}>
+                      <NavigationMenuItem
+                        className={cn("px-5 py-2 cursor-pointer rounded-xl", {
+                          "dark:bg-gray-700 bg-gray-300": isActive,
+                          "hover:dark:bg-gray-600 hover:bg-gray-200": !isActive,
+                        })}
+                      >
+                        <Icon size={18} fill={isActive ? "#131313" : "none"} />
+                      </NavigationMenuItem>
+                    </Link>
+                  </TooltipTrigger>
+
+                  <TooltipContent
+                    side="right"
+                    className="bg-gray-50 dark:bg-gray-700 rounded-xl"
+                  >
+                    <p>{label}</p>
+                  </TooltipContent>
+                </Tooltip>
+              );
+            })}
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
     </div>
   );
 };

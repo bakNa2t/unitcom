b395faf765b023bcfbea2764f6aab78b3af7ba3c
bakNa2t <ok2.lock@gmail.com>
2025-02-17 14:49:56 +0300

    Add isRouteActivePath fn

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,14 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function isRouteActivePath(currentPath: string, targetPath: string) {
+  if (targetPath === "/") {
+    return currentPath === "/" || currentPath.includes("chats");
+  }
+
+  return currentPath === targetPath || currentPath.includes(targetPath);
 }

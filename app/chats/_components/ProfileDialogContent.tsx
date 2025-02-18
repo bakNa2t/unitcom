1082f2aa5617858c7b0e39420b163e59e6bacc9d
bakNa2t <ok2.lock@gmail.com>
2025-02-18 12:20:19 +0300

    Add Input and Separator

--- app/chats/_components/ProfileDialogContent.tsx
+++ app/chats/_components/ProfileDialogContent.tsx
@@ -3,6 +3,8 @@ import { UserRound } from "lucide-react";
 
 import { Card, CardTitle } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
 
 const statuses = [
   "😎 Speak Freely",
@@ -17,20 +19,33 @@ const ProfileDialogContent = () => {
 
   return (
     <div>
-      <Card className="border-0">
+      <Card className="flex flex-col space-y-4 border-0">
         <CardTitle>Profile</CardTitle>
 
         <div>
-          <Avatar>
+          <Avatar className="w-16 h-16 mx-auto">
             <AvatarImage src="https://github.com/shadcn.png" />
             <AvatarFallback>User</AvatarFallback>
           </Avatar>
         </div>
       </Card>
 
-      <div className="flex flex-col gap-y-6">
+      <div className="flex flex-col gap-y-6 mt-4">
         <div className="flex items-center space-x-4">
           <UserRound />
+          <Input
+            value={"Username"}
+            placeholder="Name"
+            disabled
+            className="border-none outline-none ring-0"
+          />
+        </div>
+
+        <Separator />
+
+        <div className="flex items-center justify-center space-x-6">
+          <p>Manage your profile</p>
+          <button>User Button</button>
         </div>
       </div>
     </div>

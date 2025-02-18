7f2a94c048b8cbe3100fd55a9fe4a65ccd992be5
bakNa2t <ok2.lock@gmail.com>
2025-02-18 14:09:25 +0300

    Add Form to send friend's request

--- app/chats/_components/ProfileDialogContent.tsx
+++ app/chats/_components/ProfileDialogContent.tsx
@@ -9,6 +9,16 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
 
 const statuses = [
   "😎 Speak Freely",
@@ -18,19 +28,23 @@ const statuses = [
   "⌛ Taking a break",
 ];
 
-const requestFreiendFormSchema = z.object({
+const requestFriendFormSchema = z.object({
   email: z.string().min(1, { message: "Email is required" }).email(),
 });
 
 const ProfileDialogContent = () => {
   const { setTheme } = useTheme();
 
-  const form = useForm<z.infer<typeof requestFreiendFormSchema>>({
-    resolver: zodResolver(requestFreiendFormSchema),
+  const form = useForm<z.infer<typeof requestFriendFormSchema>>({
+    resolver: zodResolver(requestFriendFormSchema),
     defaultValues: {
       email: "",
     },
   });
+
+  async function onSubmit({ email }: z.infer<typeof requestFriendFormSchema>) {
+    console.log(email);
+  }
 
   return (
     <div>
@@ -73,7 +87,39 @@ const ProfileDialogContent = () => {
             </div>
           </DialogTrigger>
 
-          <DialogContent></DialogContent>
+          <DialogContent>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
+              >
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="guest@mail.com"
+                          disabled={true}
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormDescription>
+                        Enter your friend&apos;s email to send an invite
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <Button type="submit" disabled={true}>
+                  Submit
+                </Button>
+              </form>
+            </Form>
+          </DialogContent>
         </Dialog>
       </div>
     </div>

cbae71bc0ffdfab559fcfcede69129ced8d3846c
bakNa2t <ok2.lock@gmail.com>
2025-02-21 15:42:21 +0300

    Add convex friend req accept handler

--- convex/friend_request.ts
+++ convex/friend_request.ts
@@ -100,3 +100,51 @@ export const decline = mutation({
     await ctx.db.delete(args.id);
   },
 });
+
+export const accept = mutation({
+  args: {
+    id: v.id("friend_requests"),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) throw new ConvexError("Not authenticated");
+
+    const currentUser = await getUserDataById({
+      ctx,
+      clerkId: identity.subject,
+    });
+
+    if (!currentUser) throw new ConvexError("User not found");
+
+    const currentFriendRequest = await ctx.db.get(args.id);
+
+    if (
+      !currentFriendRequest ||
+      currentFriendRequest.receiver !== currentUser._id
+    )
+      throw new ConvexError("Invalid friend request");
+
+    const conversationId = await ctx.db.insert("conversations", {
+      isGroup: false,
+    });
+
+    await ctx.db.insert("contacts", {
+      user1: currentUser._id,
+      user2: currentFriendRequest.sender,
+      conversationId,
+    });
+
+    await ctx.db.insert("conversation_members", {
+      memberId: currentUser._id,
+      conversationId,
+    });
+
+    await ctx.db.insert("conversation_members", {
+      memberId: currentFriendRequest.sender,
+      conversationId,
+    });
+
+    await ctx.db.delete(args.id);
+  },
+});

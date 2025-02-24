50a830e10eebc46ae9433c3c58a940cd3d0ef97d
bakNa2t <ok2.lock@gmail.com>
2025-02-24 15:13:38 +0300

    Add friend request details

--- app/chats/_components/FriendRequestCard.tsx
+++ app/chats/_components/FriendRequestCard.tsx
@@ -1,6 +1,9 @@
 import { FC } from "react";
 import { ConvexError } from "convex/values";
+import { Handshake } from "lucide-react";
 import { toast } from "sonner";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 import { Id } from "@/convex/_generated/dataModel";
 import { api } from "@/convex/_generated/api";
@@ -48,5 +51,22 @@ export const FriendRequestCard: FC<FriendRequestCardProps> = ({
     }
   };
 
-  return <></>;
+  return (
+    <div className="flex items-center justify-between space-x-4 rounded-md border p-4">
+      <div className="flex items-center space-x-3">
+        <Handshake />
+        <Avatar>
+          <AvatarImage src={imageUrl} />
+          <AvatarFallback>{username.slice(0, 1)}</AvatarFallback>
+        </Avatar>
+
+        <div className="flex items-center justify-between">
+          <div className="flex-1 space-y-1">
+            <p className="text-sm font-medium leading-none">{username}</p>
+            <p className="text-sm text-muted-foreground">{email}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };

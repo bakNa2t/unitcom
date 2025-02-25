7906d2b089afa762a496f0d55b36c65471c31180
bakNa2t <ok2.lock@gmail.com>
2025-02-25 11:27:33 +0300

    Create SidebarContainer comp

---
+++ components/SidebarContainer.tsx
@@ -0,0 +1,35 @@
+import { FC, ReactNode } from "react";
+import { ScrollArea } from "./ui/scroll-area";
+import { Search } from "lucide-react";
+
+type SidebarContainerProps = {
+  children: ReactNode;
+  title: string;
+  trigger: ReactNode;
+};
+
+export const SidebarContainer: FC<SidebarContainerProps> = ({
+  children,
+  title,
+  trigger,
+}) => {
+  return (
+    <ScrollArea className="h-full">
+      <div className="px-4">
+        <div className="flex items-center justify-between mt-10">
+          <h2 className="text-2xl font-bold">{title}</h2>
+          <div>{trigger}</div>
+        </div>
+
+        <div className="flex items-center my-4 h-8 p-2 bg-gray-200 dark:bg-gray-800 rounded-xl">
+          <Search className="text-gray-500 mr-2" />
+          <input
+            type="text"
+            placeholder="Search..."
+            className="w-full h-8 p-2 rounded-lg focus:outline-none bg-gray-200 dark:bg-gray-800"
+          />
+        </div>
+      </div>
+    </ScrollArea>
+  );
+};

ccdf4cbd14c90f77df1957c5b4363fc235e1effb
bakNa2t <ok2.lock@gmail.com>
2025-02-26 16:10:33 +0300

    Add Form to set new group

--- app/chats/_components/ChatNewGroup.tsx
+++ app/chats/_components/ChatNewGroup.tsx
@@ -1,8 +1,32 @@
 import { useQuery } from "convex/react";
+import { useForm } from "react-hook-form";
+import { Users } from "lucide-react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-import { useMutationHandler } from "@/hooks/useMutationHandler";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 import { api } from "@/convex/_generated/api";
+import { useMutationHandler } from "@/hooks/useMutationHandler";
+
+const CreateChatNewGroupSchema = z.object({
+  name: z.string().min(2, {
+    message: "Group name must be at least 2 characters long",
+  }),
+  members: z.array(z.string()).min(2, {
+    message: "Group must have at least 2 members",
+  }),
+});
 
 export const ChatNewGroup = () => {
   const contacts = useQuery(api.contacts.get);
@@ -10,5 +34,47 @@ export const ChatNewGroup = () => {
   const { mutate: createChatGroup, state: createChatGroupState } =
     useMutationHandler(api.contacts.createGroup);
 
-  return <div>ChatNewGroup</div>;
+  const form = useForm<z.infer<typeof CreateChatNewGroupSchema>>({
+    resolver: zodResolver(CreateChatNewGroupSchema),
+    defaultValues: {
+      name: "",
+      members: [],
+    },
+  });
+
+  return (
+    <div>
+      <Dialog>
+        <DialogTrigger className="w-full">
+          <Users size={20} className="cursor-pointer" />
+        </DialogTrigger>
+        <DialogContent>
+          <Form {...form} onSubmit={form.handleSubmit(() => {})}>
+            <form className="">
+              <fieldset>
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Group name..." {...field} />
+                      </FormControl>
+
+                      <FormDescription>
+                        This is the name of the group
+                      </FormDescription>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </fieldset>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 };

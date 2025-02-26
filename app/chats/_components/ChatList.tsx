cd881163f9168e9b9dab0e13f51b9bd993dc01f2
bakNa2t <ok2.lock@gmail.com>
2025-02-26 11:57:59 +0300

    Add ChatListItem and pass comp props

--- app/chats/_components/ChatList.tsx
+++ app/chats/_components/ChatList.tsx
@@ -2,13 +2,11 @@ import { FC } from "react";
 import { usePathname } from "next/navigation";
 import { useQuery } from "convex/react";
 
+import { ChatListItem } from "./ChatListItem";
+
 import { api } from "@/convex/_generated/api";
 
-// type ChatListProps = {
-//   children: ReactNode;
-// };
-
-export const ChatList: FC<> = () => {
+export const ChatList: FC = () => {
   const pathname = usePathname();
   const chatId = pathname.split("/").pop();
 
@@ -32,8 +30,40 @@ export const ChatList: FC<> = () => {
         <div className="text-center text-gray-500">No conversations found</div>
       ) : (
         <>
-          {directMessages && directMessages.length > 0 ? <></> : null}
-          {groupMessages && groupMessages.length > 0 ? <></> : null}
+          {directMessages && directMessages.length > 0
+            ? directMessages.map(
+                ({ conversation, otherMember, unseenCount, lastMessage }) => (
+                  <ChatListItem
+                    key={conversation._id}
+                    name={otherMember?.username || ""}
+                    lastMessageContent={lastMessage?.lastMessageContent || ""}
+                    avatarUrl={otherMember?.imageUrl || ""}
+                    chatId={conversation._id}
+                    isActive={conversation._id === chatId}
+                    lastMessageSender={lastMessage?.lastMessageSender}
+                    timestamp={lastMessage?.lastMessageTimestamp}
+                    unseenMessageCount={unseenCount}
+                  />
+                )
+              )
+            : null}
+          {groupMessages && groupMessages.length > 0
+            ? groupMessages.map(
+                ({ conversation, unseenCount, lastMessage }) => (
+                  <ChatListItem
+                    key={conversation._id}
+                    name={conversation.name || ""}
+                    lastMessageContent={lastMessage?.lastMessageContent || ""}
+                    avatarUrl={""}
+                    chatId={conversation._id}
+                    isActive={conversation._id === chatId}
+                    lastMessageSender={lastMessage?.lastMessageSender}
+                    timestamp={lastMessage?.lastMessageTimestamp}
+                    unseenMessageCount={unseenCount}
+                  />
+                )
+              )
+            : null}
         </>
       )}
     </div>

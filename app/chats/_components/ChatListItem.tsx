5d3193f56b77774196e101dd70ac787d0dcede30
bakNa2t <ok2.lock@gmail.com>
2025-02-26 12:09:11 +0300

    Add sender info to display

--- app/chats/_components/ChatListItem.tsx
+++ app/chats/_components/ChatListItem.tsx
@@ -1,16 +1,57 @@
 import { FC } from "react";
+import Link from "next/link";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+import { cn } from "@/lib/utils";
 
 type ChatListItemProps = {
   name: string;
-  lastMessageContent: string | undefined;
+  lastMessageContent: string | string[];
   lastMessageSender: string | undefined;
-  timestamp: string | undefined;
+  timestamp: number | undefined;
   avatarUrl?: string | undefined;
   isActive: boolean;
   chatId: string;
   unseenMessageCount: number | undefined;
 };
 
-export const ChatListItem: FC<ChatListItemProps> = () => {
-  return <div>ChatListItem</div>;
+export const ChatListItem: FC<ChatListItemProps> = ({
+  name,
+  lastMessageContent,
+  lastMessageSender,
+  timestamp,
+  avatarUrl,
+  isActive,
+  chatId,
+  unseenMessageCount,
+}) => {
+  return (
+    <Link
+      href={`/chats/${chatId}`}
+      className={cn("flex justify-between p-3 rounded-2xl", {
+        "bg-gray-200 dark:bg-gray-800": isActive,
+      })}
+    >
+      <div className="flex space-x-3">
+        <Avatar className="w-12 h-12">
+          <AvatarImage src={avatarUrl} />
+          <AvatarFallback>{name[0]}</AvatarFallback>
+        </Avatar>
+
+        <div className="flex items-center space-y-1">
+          <h2 className="font-bold text-sm">{name}</h2>
+          <p className="text-sm text-gray-700 dark:text-gray-400">
+            {lastMessageContent}
+          </p>
+        </div>
+
+        <div className="flex flex-col items-end justify-between">
+          <p className="text-sm text-muted-foreground">
+            {timestamp ? new Date(timestamp).toLocaleTimeString() : ""}
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
 };

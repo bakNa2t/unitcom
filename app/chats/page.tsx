5fd9896580fcce3cc23fd97a560800cf45697be9
bakNa2t <ok2.lock@gmail.com>
2025-02-27 14:56:01 +0300

    Add ChatNewGroup to trigger prop

--- app/chats/page.tsx
+++ app/chats/page.tsx
@@ -1,9 +1,18 @@
+"use client";
+
+import { FaSignalMessenger } from "react-icons/fa6";
+
+import { ChatNewGroup } from "./_components/ChatNewGroup";
 import { NavigationBar } from "./_components/NavigationBar";
 
 const ChatPage = () => {
   return (
     <>
-      <NavigationBar trigger={<></>} />
+      <NavigationBar trigger={<ChatNewGroup />} />
+
+      <div className="hidden md:grid max-w-56 text-center mx-auto place-content-center">
+        <FaSignalMessenger className="text-6xl text-gray-400" />
+      </div>
     </>
   );
 };

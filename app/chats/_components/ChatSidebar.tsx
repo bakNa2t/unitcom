8ebef68b0a07acb58bf068979b23e48d8e8b0636
bakNa2t <ok2.lock@gmail.com>
2025-02-28 09:46:21 +0300

    Add ChatNewGroup

--- app/chats/_components/ChatSidebar.tsx
+++ app/chats/_components/ChatSidebar.tsx
@@ -1,9 +1,10 @@
 import { SidebarContainer } from "@/app/chats/_components/SidebarContainer";
 import { ChatList } from "./ChatList";
+import { ChatNewGroup } from "./ChatNewGroup";
 
 const ChatSidebar = () => {
   return (
-    <SidebarContainer title="Chats" trigger={<></>}>
+    <SidebarContainer title="Chats" trigger={<ChatNewGroup />}>
       <ChatList />
     </SidebarContainer>
   );

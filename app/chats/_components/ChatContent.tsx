dff287e8db4d16156512d97615791c8795e8c795
bakNa2t <ok2.lock@gmail.com>
2025-03-01 12:38:56 +0300

    Add data using convex useQuery

--- app/chats/_components/ChatContent.tsx
+++ app/chats/_components/ChatContent.tsx
@@ -1,9 +1,32 @@
-import { Id } from "@/convex/_generated/dataModel";
 import { FC } from "react";
+import { useQuery } from "convex/react";
+import { useUser } from "@clerk/clerk-react";
+
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useMutationHandler } from "@/hooks/useMutationHandler";
 
 export const ChatContent: FC<{ chatId: Id<"conversations"> }> = ({
   chatId,
 }) => {
+  const conversation = useQuery(api.conversation.get, { id: chatId });
+
+  const messages = useQuery(api.messages.get, {
+    id: chatId as Id<"conversations">,
+  });
+
+  const members = conversation?.isGroup
+    ? (conversation?.otherMembers ?? [])
+    : conversation?.otherMember
+      ? [conversation.otherMember]
+      : [];
+
+  const { mutate: markAsReed, state } = useMutationHandler(
+    api.conversation.markAsRead
+  );
+
+  const { user } = useUser();
+
   return (
     <>
       <h1 className="text-3xl font-bold">Welcome to {chatId}</h1>

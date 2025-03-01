bec66fecb38c4f15f35af969d16e0bdc4fad6029
bakNa2t <ok2.lock@gmail.com>
2025-03-01 12:27:35 +0300

    Add ChatContent and params prop

--- app/chats/[chatId]/page.tsx
+++ app/chats/[chatId]/page.tsx
@@ -2,13 +2,22 @@
 
 import { ChatNewGroup } from "../_components/ChatNewGroup";
 import { NavigationBar } from "../_components/NavigationBar";
+import { ChatContent } from "../_components/ChatContent";
 
-const ChatId = () => {
+import { Id } from "@/convex/_generated/dataModel";
+
+const ChatId = ({
+  params: { chatId },
+}: {
+  params: { chatId: Id<"conversations"> };
+}) => {
   return (
     <>
       <div className="hidden md:block">
         <NavigationBar trigger={<ChatNewGroup />} />
       </div>
+
+      <ChatContent chatId={chatId} />
     </>
   );
 };

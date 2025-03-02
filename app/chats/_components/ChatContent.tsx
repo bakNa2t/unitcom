766e13279fc0cdee78955b84124204f4d2cea17f
bakNa2t <ok2.lock@gmail.com>
2025-03-02 16:19:47 +0300

    Add ChatHeader with props

--- app/chats/_components/ChatContent.tsx
+++ app/chats/_components/ChatContent.tsx
@@ -2,6 +2,8 @@ import { FC } from "react";
 import { useQuery } from "convex/react";
 import { useUser } from "@clerk/clerk-react";
 
+import { ChatHeader } from "./ChatHeader";
+
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useMutationHandler } from "@/hooks/useMutationHandler";
@@ -27,9 +29,24 @@ export const ChatContent: FC<{ chatId: Id<"conversations"> }> = ({
 
   const { user } = useUser();
 
+  if (!conversation) return null;
+
+  const chatAvatar = conversation?.otherMember?.imageUrl || "";
+  const name = conversation?.isGroup
+    ? conversation.name
+    : conversation?.otherMember?.username || "";
+  const status = conversation?.otherMember?.status || "";
+
   return (
-    <>
+    <div className="flex w-full">
       <h1 className="text-3xl font-bold">Welcome to {chatId}</h1>
-    </>
+      <ChatHeader
+        username={name}
+        chatAvatr={chatAvatar}
+        isGruop={conversation?.isGroup}
+        chatId={chatId}
+        status={status}
+      />
+    </div>
   );
 };

af04e80df013d4d956b16a56f465282935575ac3
bakNa2t <ok2.lock@gmail.com>
2025-03-03 10:07:29 +0300

    Add Link btn to redirect to chats route

--- app/chats/_components/ChatHeader.tsx
+++ app/chats/_components/ChatHeader.tsx
@@ -1,5 +1,57 @@
 "use client";
 
-export const ChatHeader = () => {
-  return <div>Chat Header</div>;
+import { FC } from "react";
+import { useQuery } from "convex/react";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { ChevronLeft } from "lucide-react";
+
+import { api } from "@/convex/_generated/api";
+import { cn } from "@/lib/utils";
+import { useIsDesktop } from "@/hooks/useIsDesktop";
+import { useSidebarWidth } from "@/hooks/useSidebarWidth";
+
+type ChatHeaderProps = {
+  chatAvatar: string;
+  username: string;
+  chatId: string;
+  status: string;
+  isGroup: boolean;
+};
+
+export const ChatHeader: FC<ChatHeaderProps> = ({
+  chatAvatar,
+  username,
+  chatId,
+  status,
+  isGroup,
+}) => {
+  const isDesktop = useIsDesktop();
+  const { sidebarWidth } = useSidebarWidth();
+
+  const conversations = useQuery(api.conversations.get);
+
+  const groupIsCommon = conversations?.filter(
+    ({ conversation }) => conversation.isGroup
+  );
+
+  return (
+    <div
+      className={cn(
+        "fixed flex items-center justify-between w-full h-20 top-0 bg-white dark:bg-gray-800 px-3 md:pr-10 space-x-3 x-30"
+      )}
+      style={isDesktop ? { width: `calc(100% - ${sidebarWidth + 3}%)` } : {}}
+    >
+      <div className="flex space-x-3">
+        <div className="md:hidden">
+          <Button variant="outline" size="icon" asChild>
+            <Link href="/chats">
+              <ChevronLeft />
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };

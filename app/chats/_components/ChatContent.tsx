aa067a543534333770490970b288be1255d0b118
bakNa2t <ok2.lock@gmail.com>
2025-03-06 10:42:05 +0300

    Add getSeenMessage fn and props to ChatMessageItem

--- app/chats/_components/ChatContent.tsx
+++ app/chats/_components/ChatContent.tsx
@@ -7,6 +7,7 @@ import { ChatHeader } from "./ChatHeader";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useMutationHandler } from "@/hooks/useMutationHandler";
+import { ChatMessageItem } from "./ChatMessageItem";
 
 export const ChatContent: FC<{ chatId: Id<"conversations"> }> = ({
   chatId,
@@ -29,24 +30,51 @@ export const ChatContent: FC<{ chatId: Id<"conversations"> }> = ({
 
   const { user } = useUser();
 
+  const getSeenMessage = (messageId: Id<"messages">) => {
+    const seenUsers = members
+      .filter((member) => member.lastSeenMessageId === messageId)
+      .map((member) => member.username?.split(" ")[0]);
+
+    if (seenUsers.length === 0) return undefined;
+
+    return formatSeenBy(seenUsers);
+  };
+
   if (!conversation) return null;
 
-  const chatAvatar = conversation?.otherMember?.imageUrl || "";
+  const chatAvatar = conversation?.otherMember?.imageUrl ?? "";
   const name = conversation?.isGroup
-    ? conversation.name
-    : conversation?.otherMember?.username || "";
+    ? conversation?.name
+    : (conversation?.otherMember?.username ?? "");
   const status = conversation?.otherMember?.status || "";
 
   return (
     <div className="flex w-full">
-      <h1 className="text-3xl font-bold">Welcome to {chatId}</h1>
       <ChatHeader
-        username={name}
-        chatAvatr={chatAvatar}
-        isGruop={conversation?.isGroup}
+        username={name!}
+        chatAvatar={chatAvatar}
+        isGroup={conversation?.isGroup}
         chatId={chatId}
         status={status}
       />
+
+      <div className="flex flex-col-reverse gap-2 p-3">
+        {messages?.map((message, index) => (
+          <ChatMessageItem
+            key={message._id}
+            content={message.content}
+            createdAt={message._creationTime}
+            lastByUser={messages[index - 1]?.senderId === message.senderId}
+            fromCurrentUser={message.isCurrentUser}
+            senderImage={message.senderImage}
+            senderName={message.senderName}
+            type={message.type}
+            seen={
+              message.isCurrentUser ? getSeenMessage(message._id) : undefined
+            }
+          />
+        ))}
+      </div>
     </div>
   );
 };

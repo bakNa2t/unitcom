da96a22165615b7c6a60bfc4b46b4e2d01123dea
bakNa2t <ok2.lock@gmail.com>
2025-03-10 16:27:24 +0300

    Update next cfg

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "uozgqnzjuoobjshazwxx.supabase.co",
+      },
+    ],
+  },
+};
 
 export default nextConfig;

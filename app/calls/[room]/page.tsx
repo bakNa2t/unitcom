51da7cd6294abd2824a4b455b34f66c5573d9cce
bakNa2t <ok2.lock@gmail.com>
2025-03-13 10:48:12 +0300

    Add MeetingRoom with chatId param

--- app/calls/[room]/page.tsx
+++ app/calls/[room]/page.tsx
@@ -1,11 +1,16 @@
+"use client";
+
 import { FC } from "react";
 
 import { NavigationBar } from "@/app/chats/_components/NavigationBar";
+import { MeetingRoom } from "@/app/calls/_components/MeetingRoom";
 
 const Room: FC<{ params: { room: string } }> = ({ params: { room } }) => {
   return (
     <>
       <NavigationBar trigger={null} />
+
+      <MeetingRoom chatId={room} />
     </>
   );
 };

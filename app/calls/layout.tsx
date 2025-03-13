cc238f9a576309753bbf653f07aeffcfbf35ca9f
bakNa2t <ok2.lock@gmail.com>
2025-03-13 13:12:24 +0300

    Create CallsLayout

---
+++ app/calls/layout.tsx
@@ -0,0 +1,15 @@
+import { FC, ReactNode } from "react";
+
+const CallsLayout: FC<{ children: ReactNode }> = ({ children }) => {
+  return (
+    <>
+      <div className="hidden md:flex md:ml-24 px-2 md:px-0 h-dvh">
+        {children}
+      </div>
+
+      <div className="md:hidden my-20 md:px-2">{children}</div>
+    </>
+  );
+};
+
+export default CallsLayout;

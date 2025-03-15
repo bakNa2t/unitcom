dc2befdb5d69a14e46911d9f79230eba64483db1
bakNa2t <ok2.lock@gmail.com>
2025-03-15 17:14:15 +0300

    Update images remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "uozgqnzjuoobjshazwxx.supabase.co",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
     ],
   },
 };

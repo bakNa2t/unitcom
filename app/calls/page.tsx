9c218e363a4fe559d9d5585b018a1d0025ee91cd
bakNa2t <ok2.lock@gmail.com>
2025-03-16 09:50:54 +0300

    Add CallContent comp

--- app/calls/page.tsx
+++ app/calls/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import { NavigationBar } from "../chats/_components/NavigationBar";
-import { ChatNewGroup } from "../chats/_components/ChatNewGroup";
+import { NavigationBar } from "@/app/chats/_components/NavigationBar";
+import { ChatNewGroup } from "@/app/chats/_components/ChatNewGroup";
+import { CallContent } from "@/app/calls/_components/CallContent";
 
 const CallsPage = () => {
   return (
     <>
       <NavigationBar trigger={<ChatNewGroup />} />
+      <CallContent />
     </>
   );
 };

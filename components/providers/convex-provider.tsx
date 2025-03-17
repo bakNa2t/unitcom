4cd0632f327a63b356548b845dea6bc2a8b6b2c7
bakNa2t <ok2.lock@gmail.com>
2025-03-17 10:28:10 +0300

    Add heading and Card comp with sign Button

--- components/providers/convex-provider.tsx
+++ components/providers/convex-provider.tsx
@@ -10,6 +10,9 @@ import {
 } from "convex/react";
 import { FaSignalMessenger } from "react-icons/fa6";
 
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export const ConvexClientProvider = ({ children }: { children: ReactNode }) => {
@@ -21,11 +24,24 @@ export const ConvexClientProvider = ({ children }: { children: ReactNode }) => {
         <Authenticated>{children}</Authenticated>
         <Unauthenticated>
           <div className="grid place-content-center bg-slate-900 w-svw h-dvh">
-            <div>
+            <div className="flex flex-col items-center space-y-4">
               <FaSignalMessenger size={100} className="text-primary-main " />
-            </div>
 
-            <SignInButton>Sign In</SignInButton>
+              <h1 className="text-4xl font-bold text-primary-main">
+                Welcome to Unitcom!
+              </h1>
+
+              <Card className="bg-slate-800 w-[350px] border-none shadow-xl">
+                <CardHeader className="text-muted-foreground">
+                  <CardTitle>Sign in to start messaging</CardTitle>
+                </CardHeader>
+                <CardContent className="flex justify-center">
+                  <Button className="w-full bg-slate-600">
+                    <SignInButton>Sign In</SignInButton>
+                  </Button>
+                </CardContent>
+              </Card>
+            </div>
           </div>
         </Unauthenticated>
       </ConvexProviderWithClerk>

638d31a4f3cf28aceb8658487bdbb868908aa36a
bakNa2t <ok2.lock@gmail.com>
2025-03-19 14:16:19 +0300

    Update z-index

--- app/chats/_components/ChatHeader.tsx
+++ app/chats/_components/ChatHeader.tsx
@@ -49,7 +49,7 @@ export const ChatHeader: FC<ChatHeaderProps> = ({
   return (
     <div
       className={cn(
-        "fixed flex items-center justify-between w-full h-20 top-0 bg-slate-50 dark:bg-slate-900 px-3 md:pr-10 space-x-3 x-30"
+        "fixed flex items-center justify-between w-full h-20 top-0 bg-slate-50 dark:bg-slate-900 px-3 md:pr-10 space-x-3 z-9999"
       )}
       style={isDesktop ? { width: `calc(100% - ${sidebarWidth + 3}%)` } : {}}
     >

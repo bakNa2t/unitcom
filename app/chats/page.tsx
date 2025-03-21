5fb71ede93acd053d611a7069a075cd17b7a4d82
bakNa2t <ok2.lock@gmail.com>
2025-03-21 10:30:15 +0300

    Add Image comp to display logo title

--- app/chats/page.tsx
+++ app/chats/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FaSignalMessenger } from "react-icons/fa6";
+import Image from "next/image";
 
 import { ChatNewGroup } from "./_components/ChatNewGroup";
 import { NavigationBar } from "./_components/NavigationBar";
@@ -11,10 +11,26 @@ const ChatPage = () => {
     <>
       <NavigationBar trigger={<ChatNewGroup />} />
 
-      <div className="hidden md:grid max-w-56 text-center mx-auto place-content-center">
-        <FaSignalMessenger size={200} className="text-6xl text-primary-main" />
-        <h2 className="text-2xl text-primary-main mt-5">Welcome friend!</h2>
-        <p className="text-sm mt-5 text-primary-main">
+      <div className="hidden md:grid max-w-96 text-center mx-auto place-content-center">
+        <Image
+          src="/logo-title-light.svg"
+          width="600"
+          height="250"
+          alt="logo-title"
+          className="dark:hidden"
+        />
+        <Image
+          src="/logo-title-dark.svg"
+          width="600"
+          height="250"
+          alt="logo-title"
+          className="hidden dark:block"
+        />
+
+        <h2 className="text-2xl font-semibold text-primary-main mt-5">
+          Welcome friend!
+        </h2>
+        <p className="text-sm mt-2 text-primary-main/70">
           Glad you joined us to Unitcom messenger. Select or create a chat to
           start messaging
         </p>

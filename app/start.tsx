5d5935fe1512ddabcd66ec6dfcc1a678149246f4
bakNa2t <ok2.lock@gmail.com>
2025-03-28 17:18:16 +0300

    Update styles for media queries

--- app/start.tsx
+++ app/start.tsx
@@ -4,23 +4,27 @@ import { SignInButton } from "@clerk/clerk-react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 
+import { useIsDesktop } from "@/hooks/useIsDesktop";
+
 export const StartPage = () => {
+  const isDesktop = useIsDesktop();
+
   return (
     <div className="grid place-content-center bg-slate-900 w-svw h-dvh">
       <div className="flex flex-col items-center space-y-8">
         <Image
           src="/logo-title-dark.svg"
-          width="400"
+          width={isDesktop ? 400 : 300}
           height="150"
           alt="logo-title"
           className="dark:hidden"
         />
 
-        <h1 className="text-4xl font-bold text-primary-main">
+        <h1 className="text-3xl md:text-4xl font-bold text-primary-main">
           Welcome to Unitcom!
         </h1>
 
-        <Card className="bg-slate-800 w-[350px] border-none shadow-xl">
+        <Card className="bg-slate-800 w-[300px] md:w-[350px] border-none shadow-xl">
           <CardHeader className="text-muted-foreground">
             <CardTitle>Sign in to start messaging</CardTitle>
           </CardHeader>

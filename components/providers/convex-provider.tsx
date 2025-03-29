2e559c7488199ce9de915febd610cba3d2f0974b
bakNa2t <ok2.lock@gmail.com>
2025-03-29 12:09:49 +0300

    Add StartPage comp

--- components/providers/convex-provider.tsx
+++ components/providers/convex-provider.tsx
@@ -1,17 +1,15 @@
 "use client";
 
 import { ReactNode } from "react";
-import { ClerkProvider, useAuth, SignInButton } from "@clerk/clerk-react";
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import {
   Authenticated,
   ConvexReactClient,
   Unauthenticated,
 } from "convex/react";
-import Image from "next/image";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+import { StartPage } from "@/app/start";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
@@ -23,32 +21,7 @@ export const ConvexClientProvider = ({ children }: { children: ReactNode }) => {
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
         <Authenticated>{children}</Authenticated>
         <Unauthenticated>
-          <div className="grid place-content-center bg-slate-900 w-svw h-dvh">
-            <div className="flex flex-col items-center space-y-8">
-              <Image
-                src="/logo-title-dark.svg"
-                width="400"
-                height="150"
-                alt="logo-title"
-                className="dark:hidden"
-              />
-
-              <h1 className="text-4xl font-bold text-primary-main">
-                Welcome to Unitcom!
-              </h1>
-
-              <Card className="bg-slate-800 w-[350px] border-none shadow-xl">
-                <CardHeader className="text-muted-foreground">
-                  <CardTitle>Sign in to start messaging</CardTitle>
-                </CardHeader>
-                <CardContent className="flex justify-center">
-                  <SignInButton mode="modal">
-                    <Button className="w-full bg-slate-600">Sign In</Button>
-                  </SignInButton>
-                </CardContent>
-              </Card>
-            </div>
-          </div>
+          <StartPage />
         </Unauthenticated>
       </ConvexProviderWithClerk>
     </ClerkProvider>

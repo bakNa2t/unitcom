2a708dd30f9e2d7a66e4d66c0bc40796428a5d8b
bakNa2t <ok2.lock@gmail.com>
2025-03-31 10:59:57 +0300

    Update trigger theme styles

--- components/theme-mode-toggle.tsx
+++ components/theme-mode-toggle.tsx
@@ -16,7 +16,10 @@ export const ThemeModeToggle = () => {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+      <DropdownMenuTrigger
+        className="bg-slate-200 dark:bg-slate-800 border-slate-400 dark:border-slate-950  hover:border-primary-main dark:hover:border-primary-main"
+        asChild
+      >
         <Button variant="outline" size="icon" className="md:px-4">
           <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />

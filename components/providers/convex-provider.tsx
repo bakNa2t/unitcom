de503e918282cf67c078e74b8d8aae7da2cdaac7
bakNa2t <ok2.lock@gmail.com>
2025-04-02 15:58:34 +0300

    Update provider's comps

--- components/providers/convex-provider.tsx
+++ components/providers/convex-provider.tsx
@@ -3,14 +3,7 @@
 import { ReactNode } from "react";
 import { ClerkProvider, useAuth } from "@clerk/clerk-react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
-import {
-  Authenticated,
-  ConvexReactClient,
-  Unauthenticated,
-} from "convex/react";
-
-import { StartPage } from "@/app/start";
-import { ThemeProvider } from "./theme-provider";
+import { ConvexReactClient } from "convex/react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
@@ -20,17 +13,7 @@ export const ConvexClientProvider = ({ children }: { children: ReactNode }) => {
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
     >
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-        <Authenticated>{children}</Authenticated>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Unauthenticated>
-            <StartPage />
-          </Unauthenticated>
-        </ThemeProvider>
+        {children}
       </ConvexProviderWithClerk>
     </ClerkProvider>
   );
